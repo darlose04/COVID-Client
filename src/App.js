94e84e081cf61d81b6a14f410dbfa5bcd541c5ab
darlose04 <zsmith126028@gmail.com>
2020-05-08 14:31:12 -0500

    create Navbar component, import to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>COVID-19 API Client</h1>
     </div>
   );
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div id="navbar" class="navbar">
+        <h1 className="logo">
+          <i class="fas fa-viruses"></i>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
