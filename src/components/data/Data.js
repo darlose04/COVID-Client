330ae03f942ed6f194d01514d76039d149377b01
darlose04 <zsmith126028@gmail.com>
2020-05-11 11:59:53 -0500

    make get request to retrieve daily report info

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -3,18 +3,23 @@ import axios from "axios";
 
 const Data = () => {
   const [data, setData] = useState([]);
+  const [dailyReport, setDailyReport] = useState([]);
 
   const getData = async () => {
     const res = await axios.get("http://localhost:4000/api/usa/coronacases");
     setData(res.data);
   };
 
+  const getDailyReport = async () => {
+    const res = await axios.get("http://localhost:4000/api/usa/dailyreport");
+    setDailyReport(res.data);
+  };
+
   useEffect(() => {
-    getData();
+    getDailyReport();
   }, []);
 
-  console.log(data);
-  // data.map((state) => console.log(state));
+  // console.log(dailyReport[0]);
 
   return (
     <div>
