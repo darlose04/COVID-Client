647e91c140c683219679b5b8303085b40cf0595a
darlose04 <zsmith126028@gmail.com>
2020-05-11 09:20:49 -0500

    add initial Data component

---
+++ src/components/data/Data.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Data = () => {
+  return (
+    <div>
+      <h2>
+        This is where the data components for charts, states, and stats will go
+      </h2>
+    </div>
+  );
+};
+
+export default Data;
