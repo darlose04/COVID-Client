06564e16791af5aac473b61c6669a36ab9e36be5
darlose04 <zsmith126028@gmail.com>
2020-05-11 13:42:52 -0500

    trying different chart style. need to figure out how to insert the data I need

--- src/components/data/Chart.js
+++ src/components/data/Chart.js
@@ -4,10 +4,60 @@ import { Line } from "react-chartjs-2";
 const Chart = ({ cases, deaths }) => {
   // console.log(deaths);
 
+  console.log(cases[0]);
+
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Cases",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+      {
+        label: "Deaths",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(215, 42, 29, 0.4)",
+        borderColor: "rgba(215, 42, 29, 0.4)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(215, 42, 29, 0.4)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(215, 42, 29, 0.4)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [45, 23, 94, 57, 52, 67, 40],
+      },
+    ],
+  };
+
   return (
     <div>
       <h1>This is where the chart will go</h1>
-      {/* <Line data={stats} /> */}
+      <Line data={data} />
     </div>
   );
 };
