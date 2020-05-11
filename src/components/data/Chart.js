92f51e0843453f3190b37b0060f7f51dd3cd4ffe
darlose04 <zsmith126028@gmail.com>
2020-05-11 13:30:49 -0500

    create initial chart component

---
+++ src/components/data/Chart.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+const Chart = ({ cases, deaths }) => {
+  // console.log(deaths);
+
+  return (
+    <div>
+      <h1>This is where the chart will go</h1>
+      {/* <Line data={stats} /> */}
+    </div>
+  );
+};
+
+export default Chart;
