72d8202a37aa3ea1504184066d4c49fef3b43124
darlose04 <zsmith126028@gmail.com>
2020-05-11 12:43:34 -0500

    add stats to list of states. may just have the one list with the stats included instead of two separate lists

--- src/components/data/CDList.js
+++ src/components/data/CDList.js
@@ -6,7 +6,11 @@ const CDList = ({ dailyReport }) => {
       <h3>States / Territories</h3>
       <ul>
         {dailyReport.map((item) => (
-          <li key={item.index}>{item.Province_State}</li>
+          <li key={item.index}>
+            {item.Province_State} <p>Cases: {item.Confirmed}</p>{" "}
+            <p>Deaths: {item.Deaths}</p>
+            <p>Recovered: {item.Recovered}</p>
+          </li>
         ))}
       </ul>
     </div>
