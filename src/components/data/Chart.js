b03a3e933647f7e95d8a557431db975824f6e94f
darlose04 <zsmith126028@gmail.com>
2020-05-12 15:23:45 -0500

    refactor Chart component, remove duplicate code

--- src/components/data/Chart.js
+++ src/components/data/Chart.js
@@ -1,38 +1,27 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Line } from "react-chartjs-2";
 
-const Chart = ({ cases, deaths }) => {
+const Chart = ({ info, label }) => {
   // create date array for chart label (x axis)
   const dateArray = [];
-  dateArray.push(Object.keys(cases[0]));
+  dateArray.push(Object.keys(info[0]));
   const chartLabel = dateArray[0].slice(7, dateArray[0].length);
 
-  const casesArray = [];
-  const deathsArray = [];
+  const infoArray = [];
 
   chartLabel.map((date) => {
-    let numCases = 0;
-    cases.map((item) => {
-      numCases += item[date];
+    let numInfo = 0;
+    info.map((item) => {
+      numInfo += item[date];
     });
-    casesArray.push(numCases);
+    infoArray.push(numInfo);
   });
-
-  chartLabel.map((date) => {
-    let numDeaths = 0;
-    deaths.map((item) => {
-      numDeaths += item[date];
-    });
-    deathsArray.push(numDeaths);
-  });
-
-  // console.log(casesArray);
 
   const data = {
     labels: chartLabel,
     datasets: [
       {
-        label: "Cases",
+        label: label,
         fill: false,
         lineTension: 0.1,
         backgroundColor: "rgba(75,192,192,0.4)",
@@ -50,28 +39,7 @@ const Chart = ({ cases, deaths }) => {
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: casesArray,
-      },
-      {
-        label: "Deaths",
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: "rgba(215, 42, 29, 0.4)",
-        borderColor: "rgba(215, 42, 29, 0.4)",
-        borderCapStyle: "butt",
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: "miter",
-        pointBorderColor: "rgba(215, 42, 29, 0.4)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(215, 42, 29, 0.4)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: deathsArray,
+        data: infoArray,
       },
     ],
   };
