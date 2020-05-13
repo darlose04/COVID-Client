011089c57f37dd6840b4cc74acc921f8f9f500f8
darlose04 <zsmith126028@gmail.com>
2020-05-13 16:24:39 -0500

    add section to display totals in different areas. Map through dailyReport info to get stats

--- src/components/data/CDList.js
+++ src/components/data/CDList.js
@@ -1,18 +1,49 @@
 import React from "react";
 
 const CDList = ({ dailyReport }) => {
+  console.log(dailyReport);
+
+  let totalCases = 0;
+  let totalDeaths = 0;
+  let totalActive = 0;
+  let totalHospitalized = 0;
+  let totalTested = 0;
+  let totalRecovered = 0;
+
+  dailyReport.map((state) => {
+    totalCases += state.Confirmed;
+    totalDeaths += state.Deaths;
+    totalActive += state.Active;
+    totalHospitalized += state.People_Hospitalized;
+    totalTested += state.People_Tested;
+    totalRecovered += state.Recovered;
+  });
+
   return (
-    <div className="state-list">
-      <h2>States / Territories</h2>
-      <ul>
-        {dailyReport.map((item) => (
-          <li key={item.index}>
-            <h3>{item.Province_State} </h3>
-            <p>Cases: {item.Confirmed}</p> <p>Deaths: {item.Deaths}</p>
-            <p>Recovered: {item.Recovered}</p>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <div className="totals">
+        <h2>U.S. Totals</h2>
+        <ul>
+          <li>Confirmed: {totalCases}</li>
+          <li>Deaths: {totalDeaths}</li>
+          <li>Active: {totalActive}</li>
+          <li>Hospitalized: {totalHospitalized}</li>
+          <li>Tested: {totalTested}</li>
+          <li>Recovered: {totalRecovered}</li>
+        </ul>
+      </div>
+      <div className="state-list">
+        <h2>States / Territories</h2>
+        <ul>
+          {dailyReport.map((item) => (
+            <li key={item.index}>
+              <h3>{item.Province_State} </h3>
+              <p>Cases: {item.Confirmed}</p> <p>Deaths: {item.Deaths}</p>
+              <p>Recovered: {item.Recovered}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
