640b30bf26cf8496ec3757a3518e27ce7dc09896
darlose04 <zsmith126028@gmail.com>
2020-05-18 10:18:05 -0500

    import Route and Switch from react-router-dom. Add routes for home and about links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Data from "./components/data/Data";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Data />
+      <Switch>
+        <Route exact path="/" render={() => <Data />} />
+        <Route exact path="/about" render={() => <About />} />
+      </Switch>
     </div>
   );
 }
