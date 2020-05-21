2955b452f2b227c9d7a8393abef302f44bc886e1
darlose04 <zsmith126028@gmail.com>
2020-05-21 16:42:29 -0500

    create object from chartLabel array and infoArray

--- src/components/data/ReChart.js
+++ src/components/data/ReChart.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from "recharts";
 
-const ReChart = () => {
-  return <div></div>;
+const ReChart = ({ info }) => {
+  const dateArray = [];
+  dateArray.push(Object.keys(info[0]));
+  const chartLabel = dateArray[0].slice(7, dateArray[0].length);
+
+  const infoArray = [];
+
+  chartLabel.map((date) => {
+    let numInfo = 0;
+    info.map((item) => {
+      numInfo += item[date];
+    });
+    infoArray.push(numInfo);
+  });
+
+  let data = {};
+  chartLabel.forEach((label, i) => (data[label] = infoArray[i]));
+  // console.log(data);
+
+  return (
+    <div>
+      <LineChart></LineChart>
+    </div>
+  );
 };
 
 export default ReChart;
