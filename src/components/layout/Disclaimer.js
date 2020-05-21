ac8d047636c202535c3fcf7b37f0dde46faa4f5e
darlose04 <zsmith126028@gmail.com>
2020-05-21 16:28:05 -0500

    edit Disclaimer

--- src/components/layout/Disclaimer.js
+++ src/components/layout/Disclaimer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Disclaimer = () => {
   return (
     <div className="disclaimer">
-      <h1>The information presented here is one day behind!</h1>
+      <h1>THE INFORMATION PRESENTED HERE IS NOT UP TO DATE!</h1>
       <h3>
         In order to get up to date statistics, visit this link:{" "}
         <a
@@ -13,6 +13,7 @@ const Disclaimer = () => {
           COVID-19 Dashboard by the Center for Systems Science and Engineering
           (CSSE) at Johns Hopkins University (JHU)
         </a>
+        .
       </h3>
     </div>
   );
