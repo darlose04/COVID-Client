91f86bb6555717d8e65cc1c9023cb277a028beff
darlose04 <zsmith126028@gmail.com>
2020-05-27 10:48:37 -0500

    add new route for Home component. move Data component to different route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Home from "./components/Home";
 import Data from "./components/data/Data";
 import About from "./components/About";
 
@@ -10,7 +11,8 @@ function App() {
     <div className="App">
       <Navbar />
       <Switch>
-        <Route exact path="/" render={() => <Data />} />
+        <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/usdata" render={() => <Data />} />
         <Route exact path="/about" render={() => <About />} />
       </Switch>
     </div>
