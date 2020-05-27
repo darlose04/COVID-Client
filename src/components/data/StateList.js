ec61c66270ee06a965e761206429c177e4eb2091
darlose04 <zsmith126028@gmail.com>
2020-05-27 14:43:21 -0500

    remove map functions, need to do it differently. add stateName to useEffect array in order to update the component based on changes in stateName value

--- src/components/data/StateList.js
+++ src/components/data/StateList.js
@@ -18,28 +18,10 @@ const StateList = ({ stateName, baseUrl }) => {
     };
 
     getStateData();
-  }, []);
+  }, [stateName]);
 
-  const dateArray = [];
-  const infoArray = [];
-  if (stateCases.length > 0 && stateDeaths.length > 0) {
-    dateArray.push(Object.keys(stateCases[0]));
-    const chartLabel = dateArray[0].slice(7, dateArray[0].length);
-
-    chartLabel.map((date) => {
-      let numInfo = 0;
-      stateCases.map((item) => {
-        numInfo += item[date];
-      });
-      infoArray.push(numInfo);
-    });
-  }
-
-  if (dateArray.length > 0) {
-    console.log(dateArray[0].slice(7, dateArray[0].length));
-  }
-
-  console.log(infoArray);
+  console.log(stateName);
+  console.log(stateCases);
 
   return (
     <div>
