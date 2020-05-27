1339e1ef6343c76c388ce2bf69cd63c48568d4a9
darlose04 <zsmith126028@gmail.com>
2020-05-27 14:19:45 -0500

    finding a way to loop through the state array in order to get statistics for each county

--- src/components/data/StateList.js
+++ src/components/data/StateList.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const StateList = ({ dailyReport }) => {
+const StateList = ({ stateName, baseUrl }) => {
+  const [stateCases, setStateCases] = useState([]);
+  const [stateDeaths, setStateDeaths] = useState([]);
+
+  useEffect(() => {
+    const getStateData = async () => {
+      const cases = await axios.get(
+        `${baseUrl}/coronacases/states/${stateName}`
+      );
+      setStateCases(cases.data);
+      const deaths = await axios.get(
+        `${baseUrl}/coronadeaths/states/${stateName}`
+      );
+      setStateDeaths(deaths.data);
+    };
+
+    getStateData();
+  }, []);
+
+  const dateArray = [];
+  const infoArray = [];
+  if (stateCases.length > 0 && stateDeaths.length > 0) {
+    dateArray.push(Object.keys(stateCases[0]));
+    const chartLabel = dateArray[0].slice(7, dateArray[0].length);
+
+    chartLabel.map((date) => {
+      let numInfo = 0;
+      stateCases.map((item) => {
+        numInfo += item[date];
+      });
+      infoArray.push(numInfo);
+    });
+  }
+
+  if (dateArray.length > 0) {
+    console.log(dateArray[0].slice(7, dateArray[0].length));
+  }
+
+  console.log(infoArray);
+
   return (
     <div>
       <h3>State Counties (If Applicable)</h3>
-      <ul>
-        {dailyReport.map((item) => (
-          <li key={item.index}>{item.Province_State}</li>
-        ))}
-      </ul>
+      <ul></ul>
     </div>
   );
 };
