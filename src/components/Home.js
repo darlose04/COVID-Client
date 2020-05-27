002e566083e187bc845216c54abd0cd8fe1ce258
darlose04 <zsmith126028@gmail.com>
2020-05-27 10:48:03 -0500

    create Home.js component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Disclaimer from "./layout/Disclaimer";
+
+const Home = () => {
+  return (
+    <div>
+      <Disclaimer />
+    </div>
+  );
+};
+
+export default Home;
