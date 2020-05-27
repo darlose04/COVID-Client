98f3d05fdd19b16603fe046d8052b30306a46441
darlose04 <zsmith126028@gmail.com>
2020-05-27 14:04:43 -0500

    check if stateName has a value

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -47,7 +47,15 @@ const Data = () => {
             <ReChart info={deaths} dataKey="Deaths" color="rgba(198,9,9,1)" />
           </div>
           <div className="filler">
-            <StateList dailyReport={dailyReport} stateName={stateName} />
+            {stateName != "" ? (
+              <StateList
+                baseUrl={baseUrl}
+                dailyReport={dailyReport}
+                stateName={stateName}
+              />
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
       ) : (
