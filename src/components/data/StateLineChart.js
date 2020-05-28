e2890e1c24ae1020c8556948c24e1f13f0ba9556
darlose04 <zsmith126028@gmail.com>
2020-05-28 11:18:31 -0500

    add initial component for State Line Charts

---
+++ src/components/data/StateLineChart.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const StateChart = () => {
+  return (
+    <div>
+      <h2>State line chart will go here</h2>
+    </div>
+  );
+};
+
+export default StateChart;
