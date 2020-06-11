18b0242c74988a22cc807fa6319beebbde9a59d3
Zach Smith <zsmith126028@gmail.com>
2020-06-11 12:46:12 -0500

    create initial component for bar chart

---
+++ src/components/data/BarChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BarChart = () => {
+  return (
+    <div>
+      <h1>There will be a bar chart of state data here</h1>
+    </div>
+  );
+};
+
+export default BarChart;
