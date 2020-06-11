509a09b5a8a78e86a4f855a91c8ea5b5bc2de25b
Zach Smith <zsmith126028@gmail.com>
2020-06-11 13:33:09 -0500

    create bar chart

--- src/components/data/StateBarChart.js
+++ src/components/data/StateBarChart.js
@@ -39,7 +39,7 @@ const StateBarChart = ({ dailyReport }) => {
 
   return (
     <div>
-      <h1>There will be a bar chart of state data here</h1>
+      <HorizontalBar data={data} />
     </div>
   );
 };
