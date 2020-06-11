ce642efd05800522a22ab53c39cfffd26d024ddd
Zach Smith <zsmith126028@gmail.com>
2020-06-11 12:56:17 -0500

    remove rechart imports

--- src/components/data/StateLineChart.js
+++ src/components/data/StateLineChart.js
@@ -1,13 +1,4 @@
 import React from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
 
 const StateChart = () => {
   return (
