afda279a22d4a37d2409ceef1aea7036d4d9ced9
Zach Smith <zsmith126028@gmail.com>
2020-06-11 13:32:16 -0500

    create chart and add data sets, change colors of bars

--- src/components/data/StateBarChart.js
+++ src/components/data/StateBarChart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HorizontalBar } from "react-chartjs-2";
 
 const StateBarChart = ({ dailyReport }) => {
   const statesArr = [];
@@ -12,7 +13,29 @@ const StateBarChart = ({ dailyReport }) => {
     deathsArr.push(report.Deaths);
   });
 
-  console.log(statesArr);
+  const data = {
+    labels: statesArr,
+    datasets: [
+      {
+        label: "Cases",
+        backgroundColor: "rgba(101,149,247,0.3)",
+        borderColor: "rgba(16,30,229,1)",
+        borderWidth: 1,
+        hoverBackgroundColor: "rgba(101,149,247,0.5)",
+        hoverBorderColor: "rgba(16,30,229,1)",
+        data: casesArr,
+      },
+      {
+        label: "Deaths",
+        backgroundColor: "rgba(239,107,109,0.2)",
+        borderColor: "rgba(198,9,9,1)",
+        borderWidth: 1,
+        hoverBackgroundColor: "rgba(239,107,109,0.5)",
+        hoverBorderColor: "rgba(198,9,9,1)",
+        data: deathsArr,
+      },
+    ],
+  };
 
   return (
     <div>
