7add614341b4211cb225128824334e124f9aba47
Zach Smith <zsmith126028@gmail.com>
2020-06-11 13:25:45 -0500

    push necessary info into arrays for charts

--- src/components/data/StateBarChart.js
+++ src/components/data/StateBarChart.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 const StateBarChart = ({ dailyReport }) => {
+  const statesArr = [];
+  const casesArr = [];
+  const deathsArr = [];
+  console.log(dailyReport);
+
+  dailyReport.map((report) => {
+    statesArr.push(report.Province_State);
+    casesArr.push(report.Confirmed);
+    deathsArr.push(report.Deaths);
+  });
+
+  console.log(statesArr);
+
   return (
     <div>
       <h1>There will be a bar chart of state data here</h1>
