863c919dc76e043e3e636595b97f381b24ac4ae0
Zach Smith <zsmith126028@gmail.com>
2020-06-17 11:11:08 -0500

    Revert "reinstall google-maps-react and just use class component instead of functional"
    This reverts commit 577838f01ab7846b1762bf10c88280b5a4f9d9be.

--- src/components/data/USMap.js
+++ src/components/data/USMap.js
@@ -1,27 +1,11 @@
-import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import React from "react";
 
-const mapStyles = {
-  width: "100%",
-  height: "100%",
+const USMap = () => {
+  return (
+    <div>
+      <h1>Google Maps US Map goes here</h1>
+    </div>
+  );
 };
 
-export class USMap extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={6}
-        style={mapStyles}
-        initialCenter={{
-          lat: 38.5266,
-          lng: -96.7265,
-        }}
-      />
-    );
-  }
-}
-
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyBGL2RWm3yRfVwSFrbqK5agHkev2GPEhSQ",
-})(USMap);
+export default USMap;
