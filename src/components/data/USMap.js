cf1e4029ba8b2f8686ab5eb9faab36612702414b
Zach Smith <zsmith126028@gmail.com>
2020-06-17 10:51:55 -0500

    uninstall google-react-maps. docs weren't great

--- src/components/data/USMap.js
+++ src/components/data/USMap.js
@@ -1,27 +1,11 @@
 import React from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
 
 const USMap = () => {
-  const mapStyles = {
-    width: "100%",
-    height: "100%",
-  };
-
   return (
     <div>
       <h1>US Map will go here</h1>
-      <Map
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: 38.5266,
-          lng: -96.7265,
-        }}
-      />
     </div>
   );
 };
 
-export default GoogleApiWrapper({
-  apiKey: "",
-})(USMap);
+export default USMap;
