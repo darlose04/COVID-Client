f0e9d7cc3b82c898509043dd7be667d4984a6786
Zach Smith <zsmith126028@gmail.com>
2020-06-23 09:09:31 -0500

    Trying to add circle in order to use circles for state data

--- src/components/data/USMap.js
+++ src/components/data/USMap.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper, Circle } from "google-maps-react";
 
 const mapStyles = {
-  width: "100%",
-  height: "100%",
+  width: "1400px",
+  height: "800px",
+  margin: "auto",
+  marginTop: "20px",
 };
 
 export class USMap extends Component {
   render() {
+    const coords = { lat: 38.5266, lng: -96.7265 };
+
     return (
       <Map
         google={this.props.google}
@@ -17,7 +21,20 @@ export class USMap extends Component {
           lat: 38.5266,
           lng: -96.7265,
         }}
-      />
+      >
+        <Circle
+          radius={1200}
+          center={coords}
+          onMouseover={() => console.log("mouseover")}
+          onClick={() => console.log("click")}
+          onMouseout={() => console.log("mouseout")}
+          strokeColor="transparent"
+          strokeOpacity={0}
+          strokeWeight={5}
+          fillColor="#FF0000"
+          fillOpacity={0.2}
+        />
+      </Map>
     );
   }
 }
