e5803ade17b2c7c57547dc922b1749039e1b3d21
Zach Smith <zsmith126028@gmail.com>
2020-06-25 11:05:06 -0500

    add link and styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,17 @@ const About = () => {
       <h1>ABOUT</h1>
       <p>
         The information presented on this site is not intended to be used for
-        medical advice or guidance. The data used is open source, provided by
-        the Center for Systems Science and Engineering (CSSE) at Johns Hopkins
-        University (JHU).
+        medical advice or guidance. The data used is open source, provided by{" "}
+        <a
+          href="https://github.com/CSSEGISandData/COVID-19"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-JHU"
+        >
+          this Github repository
+        </a>{" "}
+        created by the Center for Systems Science and Engineering (CSSE) at
+        Johns Hopkins University (JHU).
       </p>
     </section>
   );
