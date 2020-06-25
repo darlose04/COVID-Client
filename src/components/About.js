1a04dcdd1882036a3bd57943644cdf5e17f065db
Zach Smith <zsmith126028@gmail.com>
2020-06-25 10:40:41 -0500

    add p element add minor styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,13 @@ import React from "react";
 const About = () => {
   return (
     <section className="about-section">
-      <h1 className="about-h1">ABOUT</h1>
-      <article></article>
+      <h1>ABOUT</h1>
+      <p>
+        The information presented on this site is not intended to be used for
+        medical advice or guidance. The data used is open source, provided by
+        the Center for Systems Science and Engineering (CSSE) at Johns Hopkins
+        University (JHU).
+      </p>
     </section>
   );
 };
