c385f92b6ba3a662c89f41525a10432aca3ad49d
Zach Smith <zsmith126028@gmail.com>
2020-07-03 12:28:35 -0500

    generalize var names and info, add more props in order to separate cases and deaths into two charts

--- src/components/data/DailyIncreases.js
+++ src/components/data/DailyIncreases.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { Line, Bar, HorizontalBar } from "react-chartjs-2";
 
-const DailyIncreases = ({ cases, deaths }) => {
+const DailyIncreases = ({ info, label, color }) => {
   const dateArray = [];
-  dateArray.push(Object.keys(cases[0]));
+  dateArray.push(Object.keys(info[0]));
 
   const chartLabel = dateArray[0].slice(7, dateArray[0].length);
 
   const infoArray = [];
 
   for (let i = chartLabel.length - 1; i >= 0; i--) {
-    let todayCases = 0;
-    let yesterdayCases = 0;
+    let todayNums = 0;
+    let yesterdayNums = 0;
 
-    for (let k = 0; k < cases.length; k++) {
-      todayCases += cases[k][`${chartLabel[i]}`];
-      yesterdayCases += cases[k][`${chartLabel[i - 1]}`];
+    for (let k = 0; k < info.length; k++) {
+      todayNums += info[k][`${chartLabel[i]}`];
+      yesterdayNums += info[k][`${chartLabel[i - 1]}`];
     }
 
-    let dailyIncrease = todayCases - yesterdayCases;
+    let dailyIncrease = todayNums - yesterdayNums;
     infoArray.push(dailyIncrease);
   }
 
@@ -28,20 +28,20 @@ const DailyIncreases = ({ cases, deaths }) => {
     labels: chartLabel,
     datasets: [
       {
-        label: "Daily Increase in U.S. Cases",
+        label: label,
         fill: false,
         lineTension: 0.1,
-        backgroundColor: "rgba(16, 30, 229, 1)",
-        borderColor: "rgba(16, 30, 229, 1)",
+        backgroundColor: color,
+        borderColor: color,
         borderCapStyle: "butt",
         borderDash: [],
         borderDashOffset: 0.0,
         borderJoinStyle: "miter",
-        pointBorderColor: "rgba(16, 30, 229, 1)",
+        pointBorderColor: color,
         pointBackgroundColor: "#fff",
         pointBorderWidth: 1,
         pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(16, 30, 229, 1)",
+        pointHoverBackgroundColor: color,
         pointHoverBorderColor: "rgba(220,220,220,1)",
         pointHoverBorderWidth: 2,
         pointRadius: 1,
@@ -53,18 +53,11 @@ const DailyIncreases = ({ cases, deaths }) => {
 
   return (
     <div>
-      <h1>Charts and stats for daily increases</h1>
       <Line
         data={data}
         options={{ maintainAspectRatio: true }}
         width={830}
         height={490}
-      />
-      <Bar
-        data={data}
-        options={{ maintainAspectRatio: true }}
-        width={830}
-        height={400}
       />
     </div>
   );
