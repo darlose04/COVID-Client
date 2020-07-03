2c30fdd879d003ca74793fe52ce76a05b60bec80
Zach Smith <zsmith126028@gmail.com>
2020-07-03 11:00:03 -0500

    trying different types of charts to display the daily increase info

--- src/components/data/DailyIncreases.js
+++ src/components/data/DailyIncreases.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Line, Bar, HorizontalBar } from "react-chartjs-2";
 
 const DailyIncreases = ({ cases, deaths }) => {
   const dateArray = [];
@@ -21,11 +22,50 @@ const DailyIncreases = ({ cases, deaths }) => {
     infoArray.push(dailyIncrease);
   }
 
-  console.log(infoArray);
+  console.log(infoArray.reverse());
+
+  const data = {
+    labels: chartLabel,
+    datasets: [
+      {
+        label: "Daily Increase in Cases",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(16, 30, 229, 1)",
+        borderColor: "rgba(16, 30, 229, 1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(16, 30, 229, 1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(16, 30, 229, 1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: infoArray,
+      },
+    ],
+  };
 
   return (
     <div>
       <h1>Charts and stats for daily increases</h1>
+      <Line
+        data={data}
+        options={{ maintainAspectRatio: true }}
+        width={830}
+        height={490}
+      />
+      <HorizontalBar
+        data={data}
+        options={{ maintainAspectRatio: true }}
+        width={830}
+        height={400}
+      />
     </div>
   );
 };
