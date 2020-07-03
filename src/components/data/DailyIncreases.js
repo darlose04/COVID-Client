db952335cbd34c1541d6c884d52b716f5671bf9f
Zach Smith <zsmith126028@gmail.com>
2020-07-03 10:11:06 -0500

    create array for dates

--- src/components/data/DailyIncreases.js
+++ src/components/data/DailyIncreases.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 const DailyIncreases = ({ cases, deaths }) => {
+  const dateArray = [];
+  dateArray.push(Object.keys(cases[0]));
+
+  const chartLabel = dateArray[0].slice(7, dateArray[0].length);
+
   return (
     <div>
       <h1>Charts and stats for daily increases</h1>
