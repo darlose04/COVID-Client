d5fcaa49f8c8dc0825aa4ad38549911307025e66
Zach Smith <zsmith126028@gmail.com>
2020-07-03 10:36:02 -0500

    create array of U.S. daily increases in cases by looping through chartLabel and cases arrays

--- src/components/data/DailyIncreases.js
+++ src/components/data/DailyIncreases.js
@@ -6,6 +6,23 @@ const DailyIncreases = ({ cases, deaths }) => {
 
   const chartLabel = dateArray[0].slice(7, dateArray[0].length);
 
+  const infoArray = [];
+
+  for (let i = chartLabel.length - 1; i >= 0; i--) {
+    let todayCases = 0;
+    let yesterdayCases = 0;
+
+    for (let k = 0; k < cases.length; k++) {
+      todayCases += cases[k][`${chartLabel[i]}`];
+      yesterdayCases += cases[k][`${chartLabel[i - 1]}`];
+    }
+
+    let dailyIncrease = todayCases - yesterdayCases;
+    infoArray.push(dailyIncrease);
+  }
+
+  console.log(infoArray);
+
   return (
     <div>
       <h1>Charts and stats for daily increases</h1>
