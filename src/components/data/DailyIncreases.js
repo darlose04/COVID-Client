a59f91f964e6a05d17f671e9895c0387440e5ca1
Zach Smith <zsmith126028@gmail.com>
2020-07-03 09:34:03 -0500

    create new component to show daily increases in cases and deaths

---
+++ src/components/data/DailyIncreases.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DailyIncreases = () => {
+  return (
+    <div>
+      <h1>Charts and stats for daily increases</h1>
+    </div>
+  );
+};
+
+export default DailyIncreases;
