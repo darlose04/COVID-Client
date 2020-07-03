3edca59371e56da0ca0ea102b373f35bd054d53e
Zach Smith <zsmith126028@gmail.com>
2020-07-03 12:20:32 -0500

    remove h1

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -4,7 +4,6 @@ import spinner from "./Spin-1s-200px.gif";
 const Spinner = () => {
   return (
     <div>
-      <h1>Loading Data...</h1>
       <img src={spinner} alt="Loading Gif" />
     </div>
   );
