dd2db96dbd12a34988f95c0abef0e430b3ed2c63
Zach Smith <zsmith126028@gmail.com>
2020-07-20 13:02:41 -0500

    The data is completely wrong. I have no clue where the issue is at this point

--- src/components/data/StateAPIRequest.js
+++ src/components/data/StateAPIRequest.js
@@ -29,26 +29,32 @@ const StateAPIRequest = ({ stateName }) => {
 
   return (
     <div>
-      <Chart
-        info={cases}
-        label={stateName + " Cases"}
-        color="rgba(16,30,229,1)"
-      />
-      <Chart
-        info={deaths}
-        label={stateName + " Deaths"}
-        color="rgba(198,9,9,1)"
-      />
-      <DailyIncreases
-        info={cases}
-        label={"Daily Increase in " + stateName + " Cases"}
-        color="rgba(16,30,229,1)"
-      />
-      <DailyIncreases
-        info={deaths}
-        label={"Daily Increase in " + stateName + " Deaths"}
-        color="rgba(198,9,9,1)"
-      />
+      {cases.length > 0 && deaths.length > 0 ? (
+        <div>
+          <Chart
+            info={cases}
+            label={stateName + " Cases"}
+            color="rgba(16,30,229,1)"
+          />
+          <Chart
+            info={deaths}
+            label={stateName + " Deaths"}
+            color="rgba(198,9,9,1)"
+          />
+          <DailyIncreases
+            info={cases}
+            label={"Daily Increase in " + stateName + " Cases"}
+            color="rgba(16,30,229,1)"
+          />
+          <DailyIncreases
+            info={deaths}
+            label={"Daily Increase in " + stateName + " Deaths"}
+            color="rgba(198,9,9,1)"
+          />
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
