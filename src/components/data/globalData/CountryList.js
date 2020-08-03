d1f60a301ae6b6c329a05042c1423f76647197df
Zach Smith <zsmith126028@gmail.com>
2020-08-03 15:13:35 -0500

    mapping through dailyReport array to display stats. Need to do some extra work to only display the country info and not each province or state in the countries that provide that data

--- src/components/data/globalData/CountryList.js
+++ src/components/data/globalData/CountryList.js
@@ -8,6 +8,26 @@ const CountryList = ({ dailyReport, handleChange }) => {
       <GlobalTotals dailyReport={dailyReport} handleChange={handleChange} />
       <div className="state-list">
         <h2>Countries</h2>
+        <ul>
+          {dailyReport.map((item) => {
+            let numCases = item.Confirmed;
+            let numDeaths = item.Deaths;
+            let numRecovered = item.Recovered;
+
+            return (
+              <li key={item.index}>
+                <h3>{item.Country_Region}</h3>
+                <p>Cases: {numWithCommas(numCases)}</p>
+                <p>Deaths: {numWithCommas(numDeaths)}</p>
+                {numRecovered ? (
+                  <p>Recovered: {numWithCommas(numRecovered)}</p>
+                ) : (
+                  <p>Recovered: {item.Recovered}</p>
+                )}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
