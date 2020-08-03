316511723f14737b0a08e2a00b42f544bddea094
Zach Smith <zsmith126028@gmail.com>
2020-08-03 15:06:12 -0500

    display total cases, deaths, active cases, and recovered cases for global data from daily report info

--- src/components/data/globalData/GlobalTotals.js
+++ src/components/data/globalData/GlobalTotals.js
@@ -1,10 +1,30 @@
 import React from "react";
 import numWithCommas from "../../../numWithCommas";
 
-const GlobalTotals = () => {
+const GlobalTotals = ({ dailyReport, handleChange }) => {
+  let totalCases = 0;
+  let totalDeaths = 0;
+  let totalActive = 0;
+  let totalRecovered = 0;
+
+  dailyReport.map((state) => {
+    totalCases += state.Confirmed;
+    totalDeaths += state.Deaths;
+    totalActive += state.Active;
+    totalRecovered += state.Recovered;
+  });
+
   return (
-    <div>
-      <h1>Global Totals</h1>
+    <div className="totals">
+      <h2 className="US-Totals" onClick={handleChange}>
+        Global Totals
+      </h2>
+      <ul>
+        <li>Confirmed: {numWithCommas(totalCases)}</li>
+        <li>Deaths: {numWithCommas(totalDeaths)}</li>
+        <li>Active: {numWithCommas(totalActive)}</li>
+        <li>Recovered: {numWithCommas(totalRecovered)}</li>
+      </ul>
     </div>
   );
 };
