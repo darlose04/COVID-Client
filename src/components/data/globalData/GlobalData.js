5376735d48924fbc8b17ece85a127098ace3bbb4
Zach Smith <zsmith126028@gmail.com>
2020-08-03 14:14:42 -0500

    import Spinner

--- src/components/data/globalData/GlobalData.js
+++ src/components/data/globalData/GlobalData.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Disclaimer from "../../layout/Disclaimer";
+import Spinner from "../../layout/Spinner";
 
 const GlobalData = () => {
   return (
