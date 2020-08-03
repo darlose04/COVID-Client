0b005134053a7368ab23822b3b45d7a48997ebe5
Zach Smith <zsmith126028@gmail.com>
2020-08-03 14:47:00 -0500

    create components for CountryList and GlobalTotals

---
+++ src/components/data/globalData/CountryList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GlobalTotals from "./GlobalTotals";
+
+const CountryList = () => {
+  return (
+    <div>
+      <h1>Country List</h1>
+    </div>
+  );
+};
+
+export default CountryList;
---
+++ src/components/data/globalData/GlobalTotals.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GlobalTotals = () => {
+  return (
+    <div>
+      <h1>Global Totals</h1>
+    </div>
+  );
+};
+
+export default GlobalTotals;
