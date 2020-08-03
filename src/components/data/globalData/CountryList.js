0d658ec56955450fd44ea0cbfac530f09f65dcbd
Zach Smith <zsmith126028@gmail.com>
2020-08-03 14:58:34 -0500

    add GlobalTotals, initial state-list

--- src/components/data/globalData/CountryList.js
+++ src/components/data/globalData/CountryList.js
@@ -2,10 +2,13 @@ import React from "react";
 import GlobalTotals from "./GlobalTotals";
 import numWithCommas from "../../../numWithCommas";
 
-const CountryList = () => {
+const CountryList = ({ dailyReport, handleChange }) => {
   return (
-    <div>
-      <h1>Country List</h1>
+    <div className="list-data">
+      <GlobalTotals dailyReport={dailyReport} handleChange={handleChange} />
+      <div className="state-list">
+        <h2>Countries</h2>
+      </div>
     </div>
   );
 };
