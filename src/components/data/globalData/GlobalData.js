c036491028e3f3b662945038a1e5aac27b11af9c
Zach Smith <zsmith126028@gmail.com>
2020-08-03 14:21:17 -0500

    add state and useEffect for api calls

--- src/components/data/globalData/GlobalData.js
+++ src/components/data/globalData/GlobalData.js
@@ -7,6 +7,26 @@ import useStateSelected from "../../../hooks/useStateSelected";
 const baseUrl = "https://www.cov-api.com/api/global";
 
 const GlobalData = () => {
+  const [cases, setCases] = useState([]);
+  const [deaths, setDeaths] = useState([]);
+  const [dailyReport, setDailyReport] = useState([]);
+  const [stateName, handleChange] = useStateSelected("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      const caseRequests = await axios.get(`${baseUrl}/coronacases`);
+      setCases(caseRequests.data);
+      const deathRequests = await axios.get(`${baseUrl}/coronadeaths`);
+      setDeaths(deathRequests.data);
+      const dailyReport = await axios.get(`${baseUrl}/dailyreport`);
+      setDailyReport(dailyReport.data);
+      setLoading(false);
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className="data-wrapper">
       <Disclaimer />
