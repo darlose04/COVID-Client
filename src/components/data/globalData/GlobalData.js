222555fb316585b15c4b24bb909589e794ab2c59
Zach Smith <zsmith126028@gmail.com>
2020-08-03 14:20:09 -0500

    import hook, add baseUrl for global routes

--- src/components/data/globalData/GlobalData.js
+++ src/components/data/globalData/GlobalData.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Disclaimer from "../../layout/Disclaimer";
 import Spinner from "../../layout/Spinner";
+import useStateSelected from "../../../hooks/useStateSelected";
+
+const baseUrl = "https://www.cov-api.com/api/global";
 
 const GlobalData = () => {
   return (
