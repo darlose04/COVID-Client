7d5b3a199188a6c14faa50af254adbc7fc884e24
Zach Smith <zsmith126028@gmail.com>
2020-08-05 13:18:13 -0500

    change to Global Totals

--- src/hooks/useCountrySelected.js
+++ src/hooks/useCountrySelected.js
@@ -4,7 +4,7 @@ function useCountrySelected(initialVal = "") {
   const [countryName, setCountryName] = useState(initialVal);
 
   const handleChange = (e) => {
-    if (e.target.innerText === "U.S. Totals") {
+    if (e.target.innerText === "Global Totals") {
       setCountryName("");
     } else {
       setCountryName(e.target.innerText);
